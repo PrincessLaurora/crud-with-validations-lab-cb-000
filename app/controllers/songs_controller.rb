fac8e3018a02b42eed309f88518e1b32b0123fb8
Laura Felce <crepu83@yahoo.it>
2019-06-04 08:23:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,47 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end  
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+    if @song.save
+        redirect_to song_path(@song)
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @post.update(post_params)
+    if @post.valid?
+        @post.save
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+  end
+
+  private
+
+ def songs_params
+   params.permit(:title, :category, :content)
+ end
+end
+
+  
 end
